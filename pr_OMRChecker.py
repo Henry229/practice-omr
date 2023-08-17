ef9c842e10c683d748bcaff2db503e95026ce5d1
henry chun <henry.chun@cseducation.com.au>
2023-08-17 19:39:17 +1000

    picking up only things recognised

--- pr_OMRChecker.py
+++ pr_OMRChecker.py
@@ -26,6 +26,11 @@ def recognize_marked_answers_using_pixel_ratio(image, bubble_coordinates, bubble
     for field, coordinates in bubble_coordinates.items():
         field_answers = []
         num_of_selection = sel_MT_block_data["fieldBlocks"][field]["num_of_selection"]
+        # Only for blocks starting with Math_Answer_ or Think_Answer_
+        if field.startswith("Math_Answer_") or field.startswith("Think_Answer_"):
+            starting_question = sel_MT_block_data["fieldBlocks"][field].get("StartingQuestion", 1)
+        else:
+            starting_question = None
 
         for idx, bubble in enumerate(coordinates):
             top_left = tuple(bubble["top_left"])
@@ -39,17 +44,20 @@ def recognize_marked_answers_using_pixel_ratio(image, bubble_coordinates, bubble
             
             # Determine the marked choice index
             choice_index = idx % num_of_selection
+            question_number = "Q" + str(int(starting_question) + idx // num_of_selection) if starting_question is not None else None
             
             # Consider the bubble as marked if the black pixel ratio is above the threshold
             answer_data = {
                 "block_name": field,
                 "is_marked": 1 if black_pixel_ratio < threshold_ratio else 0,
-                "marked_choice_index": choice_index if black_pixel_ratio < threshold_ratio else None
+                "marked_choice_index": choice_index if black_pixel_ratio < threshold_ratio else None,
+                "question_number": question_number
             }
+
             field_answers.append(answer_data)
             
         recognized_answers[field] = field_answers
-    print('>>> recognized_answers:', recognized_answers)
+
     return recognized_answers
 
 def visualize_correct_marked_answers(image, bubble_coordinates, bubble_dimensions, answers):
@@ -74,13 +82,15 @@ with open('selective_Math_Thinking_coordinates.json', 'r') as file:
     loaded_bubble_coordinates = json.load(file)
 
 # Load image
-omr_image = cv2.imread('/Users/henrychun/Downloads/Math-Thinking-NSW.jpeg')
+omr_image = cv2.imread('/Users/henry/Downloads/Math-Thinking-NSW.jpeg')
 
 # Preprocess the image
 preprocessed_image = preprocess_image_for_recognition(omr_image)
 
 # Recognize marked answers using the pre-processed image
 marked_answers = recognize_marked_answers_using_pixel_ratio(preprocessed_image, loaded_bubble_coordinates, sel_MT_block_data["bubbleDimensions"], threshold_ratio=0.75)
+filtered_answers = {block: [answer for answer in answers if answer["is_marked"] == 1] for block, answers in marked_answers.items()}
+print('>>> recognized_answers:', filtered_answers)
 
 # Visualize the correctly marked answers on the original image
 visualized_image_with_correct_markings = visualize_correct_marked_answers(omr_image, loaded_bubble_coordinates, sel_MT_block_data["bubbleDimensions"], marked_answers)
--- test.py
+++ test.py
@@ -1,159 +1,59 @@
-# import cv2
-# import numpy as np
-
-# def recognize_omr(image_path, omr_definition):
-#     # 이미지 읽기
-#     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
-#     # 전처리: GaussianBlur
-#     pre_processor = omr_definition['preProcessors'][0]
-#     if pre_processor['name'] == 'GaussianBlur':
-#         kSize = tuple(pre_processor['options']['kSize'])
-#         sigmaX = pre_processor['options']['sigmaX']
-#         image = cv2.GaussianBlur(image, kSize, sigmaX)
-
-#     # 결과를 저장할 딕셔너리
-#     results = {}
-
-#     # 각 필드 블록에 대한 버블 인식
-#     for field, details in omr_definition['fieldBlocks'].items():
-#         origin_x, origin_y = details['origin']
-#         bubbles_gap = details['bubblesGap']
-#         bubble_width, bubble_height = omr_definition['bubbleDimensions']
-
-#         field_results = []
-
-#         for idx, label in enumerate(details['fieldLabels']):
-#             x = origin_x + idx * details['labelsGap']
-#             y = origin_y
-
-#             # 버블 영역 추출
-#             bubble_area = image[y:y+bubble_height, x:x+bubble_width]
-
-#             # 버블 내부의 픽셀 값 평균 계산
-#             mean_val = np.mean(bubble_area)
-
-#             # 픽셀 값 평균이 임계값보다 낮으면 마킹으로 판단 (임계값은 조정이 필요)
-#             if mean_val < 128:
-#                 field_results.append(label)
-
-#         results[field] = field_results
-
-#     return results
-
-# # 주어진 JSON 파일을 파이썬 딕셔너리로 변환
-# omr_definition = {
-#     "pageDimensions": [1169, 1654],
-#     "bubbleDimensions": [18, 18],
-#     "preProcessors": [
-#         {
-#             "name": "GaussianBlur",
-#             "options": {
-#                 "kSize": [3, 3],
-#                 "sigmaX": 0
-#             }
-#         }
-#     ],
-#     # ... (나머지 JSON 내용)
-# }
-
-# # 예제 사용
-# image_path = "path_to_your_image.jpg"
-# print(recognize_omr(image_path, omr_definition))
-
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-def preprocess_image_for_recognition(image):
-    # 1. Gaussian blur
-    blurred = cv2.GaussianBlur(image, (5, 5), 0)
-    
-    # 2. Thresholding using OTSU's method
-    _, thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-    
-    # 3. Morphological operation: CLOSE, to close small gaps
-    kernel = np.ones((2, 2), np.uint8)
-    morphed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    
-    return morphed
-
-def recognize_marked_answers_using_pixel_ratio(image, bubble_coordinates, bubble_dimensions, threshold_ratio=0.5):
-    recognized_answers = {}
-    
-    for field, coordinates in bubble_coordinates.items():
-        field_answers = []
-        for bubble in coordinates:
-            top_left = tuple(bubble["top_left"])
-            bottom_right = tuple(bubble["bottom_right"])
-            bubble_area = image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-            
-            # Calculate the ratio of black pixels
-            total_pixels = bubble_area.size
-            black_pixels = total_pixels - cv2.countNonZero(bubble_area)
-            black_pixel_ratio = black_pixels / total_pixels
-            
-            # Consider the bubble as marked if the black pixel ratio is above the threshold
-            if black_pixel_ratio > threshold_ratio:
-                field_answers.append(1)
-            else:
-                field_answers.append(0)
-        
-        recognized_answers[field] = field_answers
-    
-    return recognized_answers
-
-def visualize_correct_marked_answers(image, bubble_coordinates, bubble_dimensions, answers):
-    visualized_image = image.copy()
-    
-    for block_name, block_coords in bubble_coordinates.items():
-        if block_name in answers:
-            for idx, coord_dict in enumerate(block_coords):
-                top_left = tuple(coord_dict["top_left"])
-                bottom_right = tuple(coord_dict["bottom_right"])
-                
-                # Check if the bubble is NOT marked
-                if answers[block_name][idx] == 0:
-                    cv2.rectangle(visualized_image, top_left, bottom_right, (0, 0, 255), 2)
-                    
-    return visualized_image
-
-# Load image
-omr_image = cv2.imread('/mnt/data/Math-Thinking-NSW.jpeg')
-
-# Preprocess the image
-preprocessed_image = preprocess_image_for_recognition(omr_image)
-
-# Recognize marked answers using the pre-processed image
-marked_answers = recognize_marked_answers_using_pixel_ratio(preprocessed_image, loaded_bubble_coordinates, template_data["bubbleDimensions"], threshold_ratio=0.75)
-
-# Visualize the correctly marked answers on the original image
-visualized_image_with_correct_markings = visualize_correct_marked_answers(omr_image, loaded_bubble_coordinates, template_data["bubbleDimensions"], marked_answers)
-
-# Show the visualized image
-plt.figure(figsize=(12, 20))
-plt.imshow(cv2.cvtColor(visualized_image_with_correct_markings, cv2.COLOR_BGR2RGB))
-plt.axis('off')
-plt.show()
-
-
-[
-    {
-        "student": "15990005",
-        "Test_Type": "Selective_Trial_Test",
-        "Test_Subj": ["Maths", "GA"],
-        "Test_Level1": "Year5",
-        "Test_Level2": "",
-        "Test_No": "07",
-        "Brnach_No": "22"
-     }
-]
-
-0100000000
-0000010000
-0000000001
-0000000001
-1000000000
-1000000000
-1000000000
-0000010000
+recognized_answers: {
+    'Student_No': [
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': None}, 
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 5, 'question_number': None}, 
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 9, 'question_number': None}, 
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 9, 'question_number': None}, 
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': None}, 
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': None}, 
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': None}, 
+        {'block_name': 'Student_No', 'is_marked': 1, 'marked_choice_index': 5, 'question_number': None}], 
+    'Test_Type': [
+        {'block_name': 'Test_Type', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': None}], 
+    'Test_Subject': [
+        {'block_name': 'Test_Subject', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': None}, 
+        {'block_name': 'Test_Subject', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': None}], 
+    'Test_Level1': [
+        {'block_name': 'Test_Level1', 'is_marked': 1, 'marked_choice_index': 4, 'question_number': None}], 
+    'Test_Level2': [], 
+    'Test_No': [
+        {'block_name': 'Test_No', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': None}, 
+        {'block_name': 'Test_No', 'is_marked': 1, 'marked_choice_index': 7, 'question_number': None}], 
+    'Branch_No': [
+        {'block_name': 'Branch_No', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': None}, 
+        {'block_name': 'Branch_No', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': None}], 
+    'Math_Answer_1': [
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q1'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q2'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q3'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q4'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 4, 'question_number': 'Q5'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q6'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q7'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q8'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q9'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q10'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q11'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q12'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 4, 'question_number': 'Q13'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q14'}, 
+        {'block_name': 'Math_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q15'}], 
+    'Math_Answer_16': [
+        {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q16'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q17'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q18'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q19'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q20'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 4, 'question_number': 'Q21'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q22'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q23'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q24'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q25'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q26'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q27'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q28'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 4, 'question_number': 'Q29'}, {'block_name': 'Math_Answer_16', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q30'}], 
+    'Math_Answer_31': [
+        {'block_name': 'Math_Answer_31', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q31'}, {'block_name': 'Math_Answer_31', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q32'}, {'block_name': 'Math_Answer_31', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q33'}, {'block_name': 'Math_Answer_31', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q34'}, {'block_name': 'Math_Answer_31', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q35'}],
+    'Think_Answer_1': [
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q1'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q2'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q3'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q4'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q5'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q6'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q7'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q8'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q9'}, 
+        {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q10'}, {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q11'}, {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q12'}, {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q13'}, {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q14'}, {'block_name': 'Think_Answer_1', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q15'}], 
+    'Think_Answer_16': [
+        {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q16'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q17'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q18'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q19'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q20'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q21'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q22'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q23'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q24'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q25'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q26'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q27'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q28'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q29'}, {'block_name': 'Think_Answer_16', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q30'}], 
+    'Think_Answer_31': [
+        {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q31'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q32'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q33'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q34'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q35'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q36'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 3, 'question_number': 'Q37'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 2, 'question_number': 'Q38'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 1, 'question_number': 'Q39'}, {'block_name': 'Think_Answer_31', 'is_marked': 1, 'marked_choice_index': 0, 'question_number': 'Q40'}]}
